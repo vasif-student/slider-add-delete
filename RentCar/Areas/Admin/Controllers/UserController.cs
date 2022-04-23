88c0fddbb728b9c4766fc9d583b7daa64f9d1d2a
vasif-student <vasifah@code.edu.az>
2022-04-23 22:14:34 +0400

    slider-add-delete

---
+++ RentCar/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentCar.Areas.Admin.Models.ViewModel;
+using RentCar.DAL;
+using RentCar.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentCar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = RoleConstants.SuperAdmin)]
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(AppDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+            var userList = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                userList.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Role = (await _userManager.GetRolesAsync(user))[0]
+                });
+            }
+            return View(userList);
+        }
+    }
+}
